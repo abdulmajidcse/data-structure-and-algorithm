ef7ef5e03f58707cdb68f2ad401ea37267bd6a9e
Abdul Majid <abdulmajidcse@gmail.com>
2024-11-18 23:17:18 +0600

    bubble sort improvement.

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,16 +1,25 @@
 // Bubble sort
-let myArray = [64, 34, 25, 12, 22, 11, 90, 5];
+// let myArray = [64, 34, 25, 12, 22, 11, 90, 5];
+let myArray = [7, 3, 9, 12, 11];
 console.log("Unsorted array = ", myArray);
 
 // outer loop run (n-1) times to sort
 for (let i = 0; i < myArray.length; i++) {
+  // to improve performance
+  let isSwapped = false;
   for (let j = 0; j < myArray.length; j++) {
     if (myArray[j] > myArray[j + 1]) {
       // swaps current and next value
       let currentValue = myArray[j];
       myArray[j] = myArray[j + 1];
       myArray[j + 1] = currentValue;
+      isSwapped = true;
     }
+  }
+
+  if (isSwapped == false) {
+    // to improve performance
+    break;
   }
 }
 
