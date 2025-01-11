e8fcb15d5a7e0649ad7575a581326711eb55b144
Abdul Majid <abdulmajidcse@gmail.com>
2025-01-11 23:21:03 +0600

    add comments.

--- quick_sort.php
+++ quick_sort.php
@@ -3,10 +3,12 @@
 function partition(&$array, $low, $high)
 {
     $pivot = $array[$high];
+    // for pivot index
     $i = $low - 1;
 
     for ($j = $low; $j < $high; $j++) {
         if ($array[$j] <= $pivot) {
+            // increment pivot index and swap value
             $i += 1;
             $temp = $array[$i];
             $array[$i] = $array[$j];
@@ -14,18 +16,23 @@ function partition(&$array, $low, $high)
         }
     }
 
+    // swap value and it's important when no swapping in iteration
     $temp = $array[$i + 1];
     $array[$i + 1] = $array[$high];
     $array[$high] = $temp;
 
+    // pivot index
     return $i + 1;
 }
 
 function quickSort(&$array, $low, $high)
 {
     if ($low < $high) {
+        // get pivot index
         $pivotIndex = partition($array, $low, $high);
+        // left side
         quickSort($array, $low, $pivotIndex - 1);
+        // right side
         quickSort($array, $pivotIndex + 1, $high);
     }
 }
