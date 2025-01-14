8859be34494ae35c1b70833a555b64dc11a5a35f
Abdul Majid <abdulmajidcse@gmail.com>
2025-01-14 22:44:02 +0600

    add a new function to sort array without recursion.

--- merge_sort.php
+++ merge_sort.php
@@ -1,27 +1,5 @@
 <?php
 
-function mergeSort(array $array): array
-{
-    $n = count($array);
-    if ($n <= 1) {
-        // array with single value
-        return $array;
-    }
-
-    // get left & right side array
-    $mid = intdiv($n, 2);
-    $leftHalf = array_slice($array, 0, $mid);
-    $rightHalf = array_slice($array, $mid);
-
-    // sort left side array
-    $sortedLeft = mergeSort($leftHalf);
-    // sort right side array
-    $sortedRight = mergeSort($rightHalf);
-
-    // sorted array
-    return merge($sortedLeft, $sortedRight);
-}
-
 function merge(array $left, array $right): array
 {
     $result = [];
@@ -46,6 +24,56 @@ function merge(array $left, array $right): array
     return $result;
 }
 
+function mergeSort(array $array): array
+{
+    $n = count($array);
+    if ($n <= 1) {
+        // array with single value
+        return $array;
+    }
+
+    // get left & right side array
+    $mid = intdiv($n, 2);
+    $leftHalf = array_slice($array, 0, $mid);
+    $rightHalf = array_slice($array, $mid);
+
+    // sort left side array
+    $sortedLeft = mergeSort($leftHalf);
+    // sort right side array
+    $sortedRight = mergeSort($rightHalf);
+
+    // sorted array
+    return merge($sortedLeft, $sortedRight);
+}
+
+function mergeSortWithoutRecursion(array $array): array
+{
+    $step = 1;
+    $n = count($array);
+
+    while ($step < $n) {
+        for ($i = 0; $i < $n; $i += 2 * $step) {
+            // get left & right array with values 1, 2, 4... length
+            $left = array_slice($array, $i, $step);
+            $right = array_slice($array, $i + $step, $step);
+
+            // merge to sort
+            $merged = merge($left, $right);
+
+            foreach ($merged as $j => $value) {
+                // replace sorted value
+                $array[$i + $j] = $value;
+            }
+        }
+
+        $step *= 2;
+    }
+
+    return $array;
+}
+
 $myArray = [3, 7, 0, 6, -10, 15, 23.5, 55, -13];
 $sortedArray = mergeSort($myArray);
 echo implode(', ', $sortedArray);
+$sortedArray2 = mergeSortWithoutRecursion($myArray);
+printf("\n%s", implode(', ', $sortedArray2));
