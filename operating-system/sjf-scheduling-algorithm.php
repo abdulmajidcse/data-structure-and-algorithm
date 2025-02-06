fe9e0e85f23b0c5c9c1cd6eaf5e85cf836f09914
abdulmajidcse <abdulmajidcse@gmail.com>
2025-02-06 22:54:50 +0600

    Add comments.

--- operating-system/sjf-scheduling-algorithm.php
+++ operating-system/sjf-scheduling-algorithm.php
@@ -15,23 +15,30 @@ for ($i = 1; $i <= $processCount; $i++) {
     $burstTimes[$p] = floatval(readline("$p = "));
 }
 
+// sort ascending order by burst time for priority
 asort($burstTimes);
+
+// time track
 $waitingTime = 0;
 $turnaroundTime = 0;
 $processHistories = [];
 foreach ($burstTimes as $key => $burstTime) {
+    // turnaround time for current process
     $turnaroundTime += $burstTime;
+    // store process history
     $processHistories[$key] = [
         'waiting_time' => $waitingTime,
         'turnaround_time' => $turnaroundTime,
     ];
 
+    // waiting time for next process
     $waitingTime += $burstTime;
 }
 
+// sort ascending order by process for display
 ksort($processHistories);
 
-
+// Waiting time information
 echo "\nWaiting Time:\n";
 $totalWaitingTime = 0;
 foreach ($processHistories as $key => $processHistory) {
@@ -41,6 +48,7 @@ foreach ($processHistories as $key => $processHistory) {
 
 echo "Average Waiting Time = " . ($totalWaitingTime / $processCount) . "\n";
 
+// Turnaround time information
 echo "\nTurnaround Time:\n";
 $totalTurnaroundTime = 0;
 foreach ($processHistories as $key => $processHistory) {
