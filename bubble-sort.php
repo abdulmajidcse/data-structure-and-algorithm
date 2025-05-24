bb99b6a6db30c53de88667fe37fe2db5d7d697c9
Md Abdul Majid <abdulmajidcse@gmail.com>
2025-05-24 17:29:07 +0600

    Optimize bubble sort.

--- bubble-sort.php
+++ bubble-sort.php
@@ -6,12 +6,18 @@ echo "Before sorted: " . implode(', ', $arr) . PHP_EOL;
 
 $n = count($arr);
 for ($i = 0; $i < $n; $i++) {
+    $isSwapped = false;
     for ($j = 0; $j < $n - $i - 1; $j++) {
         if ($arr[$j] > $arr[$j + 1]) {
             $temp = $arr[$j];
             $arr[$j] = $arr[$j + 1];
             $arr[$j + 1] = $temp;
+            $isSwapped = true;
         }
+    }
+
+    if (!$isSwapped) {
+        break;
     }
 }
 
