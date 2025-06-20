95a241cba8bb6ec0889c17eef4d9b5d264e25e70
Md Abdul Majid <abdulmajidcse@gmail.com>
2025-06-20 10:23:09 +0600

    Queue simple implementation by array

---
+++ queue/simple-queue.php
@@ -0,0 +1,23 @@
+<?php
+
+$queue = [];
+
+// Enqueue
+array_push($queue, 10);
+array_push($queue, 20);
+array_push($queue, 30);
+array_push($queue, 40);
+
+echo 'Queue: ' . implode(' ', $queue) . PHP_EOL;
+
+// Dequeue
+echo 'Dequeue: ' . array_pop($queue) . PHP_EOL;
+
+// Peek
+echo 'Peek: ' . (isset($queue[0]) ? $queue[0] : 'Queue is empty') . PHP_EOL;
+
+// isEmpty
+echo 'isEmpty: ' . (count($queue) == 0 ? 'True' : 'False') . PHP_EOL;
+
+// Size
+echo 'Size: ' . count($queue) . PHP_EOL;
