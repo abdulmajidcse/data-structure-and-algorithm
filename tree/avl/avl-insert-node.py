c5b3145493f81372f3f0db043a55b0e6a15da7db
Md Abdul Majid <abdulmajidcse@gmail.com>
2025-08-30 22:34:49 +0600

    AVL tree data insert

--- tree/avl/avl-insert-node.py
+++ tree/avl/avl-insert-node.py
@@ -41,4 +41,49 @@ def right_rotate(node):
     
     return rotate_node
 
+def insert(node, data):
+    if not node:
+        return TreeNode(data)
 
+    if data < node.data:
+        node.left = insert(node.left, data)
+    elif data > node.data:
+        node.right = insert(node.right, data)
+
+    node.height = 1 + max(get_height(node.left), get_height(node.right))
+    balance = get_balance(node)
+
+    # Left-Left
+    if balance < -1 and get_balance(node.left) <= 0:
+        return right_rotate(node)
+    
+    # Left-Right
+    if balance < -1 and get_balance(node.left) > 0:
+        node.left = left_rotate(node.left)
+        return right_rotate(node)
+    
+    # Right-Right
+    if balance > 1 and get_balance(node.right) >= 0:
+        return left_rotate(node)
+    
+    if balance > 1 and get_balance(node.right) < 0:
+        node.right = right_rotate(node.right)
+        return left_rotate(node)
+    
+    return node
+
+def inOrderTraversal(node):
+    if node is None:
+        return
+    inOrderTraversal(node.left)
+    print(node.data, end=", ")
+    inOrderTraversal(node.right)
+
+# Inserting nodes
+root = None
+letters = ['C', 'B', 'E', 'A', 'D', 'H', 'G', 'F']
+for letter in letters:
+    root = insert(root, letter)
+
+inOrderTraversal(root)
+print()
